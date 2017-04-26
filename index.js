2326793141624805c6c9fc6e352f9f2da4e24f51
tutu <yushijiechn@qq.com>
2017-04-26 17:08:22 +0800

    fix-something

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 var shpTransform = require('./lib/shpTransform');
+
 /**
  *@param originFileName
  *@param transformType(gcj2wgs、wgs2gcj、gcj2bd、wgs2bd、bd2gcj)
  */
+
 shpTransform('polygon.shp', 'wgs2bd');
